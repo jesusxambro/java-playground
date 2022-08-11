ca15bb5aaebc10da0ca086fdf6458bcabf82c80a
Jesus Ambro <jesus.ambrocio@outlook.com>
2022-08-11 13:32:07 -0500

    Added graded stuff

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -3,7 +3,25 @@ package com.galvanize;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        String helloWorld = "Hello World";
+        System.out.println(helloWorld);
+
+        int input = 91;
+        if (input < 0 || input >100){
+            return;
+        }
+
+        if(input <= 100 && input >= 90){
+            System.out.println("A");
+        } else if (input <90 && input >= 80) {
+            System.out.println("B");
+        } else if (input < 80 && input >= 70) {
+            System.out.println("C");
+        } else if (input < 70 && input >=60) {
+            System.out.println("D");
+        }else{
+            System.out.println("F");
+        }
     }
 
 }
